99831f14a39fc0c9e79595adeb1c7d2e529aa567
umar2009-hub <umarabd104@gmail.com>
2026-01-31 20:16:25 +0530

    latest commit

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,17 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-exports.generateAccessToken = (userId) => {
-  return jwt.sign(
-    { userId },
-    process.env.JWT_SECRET,
-    { expiresIn: "15m" }
-  );
+exports.generateAccessToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15m"
+  });
 };
 
-exports.generateRefreshToken = (userId) => {
-  return jwt.sign(
-    { userId },
-    process.env.JWT_REFRESH_SECRET,
-    { expiresIn: "7d" }
-  );
+exports.generateRefreshToken = (id) => {
+  return jwt.sign({ id }, process.env.REFRESH_SECRET, {
+    expiresIn: "7d"
+  });
 };
